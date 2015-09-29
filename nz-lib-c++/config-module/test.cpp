f8986f6d0ffc78001e2c79da4c95aa30daf208c5
xiliufengshui <xiliufengshui@163.com>
2015-09-29 16:30:58 +0800

    add lib of c++ or c

---
+++ nz-lib-c++/config-module/test.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// LastChangeTime : Time-stamp: <naturezhang 2015/02/16 19:50:27>
+// Name           : test.cpp
+// Version        : 1.0
+// Copyright      : 裸奔的鸡蛋
+// Author         : 张健
+// CreatDate      : 2015年02月12日 PM  5:29 Thu
+// Description    : just for test libxml2
+//============================================================================
+
+
+
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+     
+     return 0;
+}
+
