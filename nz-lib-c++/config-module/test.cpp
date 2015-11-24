f529c3160eba5a7eab078f153b41842d78ae054a
xiliufengshui <xiliufengshui@163.com>
2015-11-24 19:45:15 +0800

    add module of config

--- nz-lib-c++/config-module/test.cpp
+++ nz-lib-c++/config-module/test.cpp
@@ -1,21 +1,20 @@
 //============================================================================
-// LastChangeTime : Time-stamp: <naturezhang 2015/02/16 19:50:27>
+// LastChangeTime : Time-stamp: <naturezhang 2015/11/19 20:33:01>
 // Name           : test.cpp
 // Version        : 1.0
 // Copyright      : 裸奔的鸡蛋
 // Author         : 张健
-// CreatDate      : 2015年02月12日 PM  5:29 Thu
-// Description    : just for test libxml2
+// CreatDate      : 2015年11月19日 PM  8:28 Thu
+// Description    : just for test
 //============================================================================
 
 
-
-#include <iostream>
-
+#include "NZconfig.h"
 
 int main(int argc, char *argv[])
 {
-     
-     return 0;
+    NZconfig classConfig;
+    classConfig.read_config_file("test.ini");
+    classConfig.print_config();
+    return 0;
 }
-
