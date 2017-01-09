82be135599f72c4a1e356eea07ba898ce192016b
xiliufengshui <xiliufengshui@163.com>
2017-01-09 16:12:22 +0800

    读取配置信息

--- nz-lib-c++/text-processor-utf8/main.cpp
+++ nz-lib-c++/text-processor-utf8/main.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// LastChangeTime : Time-stamp: <naturezhang 2017/01/09 15:35:30>
+// LastChangeTime : Time-stamp: <naturezhang 2017/01/09 16:05:57>
 // Name           : main.cpp
 // Version        : 1.0
 // Copyright      : 裸奔的鸡蛋
@@ -461,11 +461,11 @@ int main(int argc, char *argv[])
 {
     // filter_msg(argv[1]);
     CTextProcessor oCTextProcessor;
-    // oCTextProcessor.init_from_configuration_file(argv[1]);
+    oCTextProcessor.init_from_configuration_file(argv[1]);
     // oCTextProcessor.reload_init_data();
-    // oCTextProcessor.get_sample_feature(argv[1]);
-    double dRst = oCTextProcessor.get_svm_predict("100元-3万金币要的加QQ14757928先货");
-    printf("predict rst: %g\n", dRst);
+    oCTextProcessor.get_sample_feature(argv[2]);
+    // double dRst = oCTextProcessor.get_svm_predict("100元-3万金币要的加QQ14757928先货");
+    // printf("predict rst: %g\n", dRst);
     // // oCTextProcessor.stat_msg_length(argv[1]);
     // oCTextProcessor.tag_msg_length(argv[1]);
     // oCTextProcessor.get_sample_feature(argv[1]);
